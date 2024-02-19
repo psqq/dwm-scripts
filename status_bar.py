fb489c6f6541d04ca157a1e409b4fb5b07c714c3
Sergey Pestov <pestov-sa@ya.ru>
2024-02-20 00:08:03 +0300

    display argument for status_bar.py

--- status_bar.py
+++ status_bar.py
@@ -10,7 +10,7 @@ import battery_charge
 import volume
 
 
-def render():
+def render(display):
     date = status_bar_date.date_for_status_bar()
     current_kblayout = kb_layout.get_kbdlayout()
     content = " | "
@@ -25,25 +25,31 @@ def render():
         content += f"V {v}% | "
     content += date
     content += " | "
-    subprocess.run(["xsetroot", "-name", content])
+    xsetrootEnv = os.environ.copy()
+    if display:
+        xsetrootEnv["DISPLAY"] = display
+    subprocess.run(["xsetroot", "-name", content], env=xsetrootEnv)
 
 
-def is_loop_runned():
+def is_loop_runned(display):
     ps = os.popen("ps aux").readlines()
     c = 0
+    cmdForSearch = "status_bar.py loop"
+    if display:
+        cmdForSearch += " " + display
     for p in ps:
-        if "status_bar.py loop" in p:
+        if cmdForSearch in p:
             c += 1
     return c > 1
 
 
-def loop():
-    if is_loop_runned():
+def loop(display):
+    if is_loop_runned(display):
         print("status bar loop alredy runned!")
         return
     while True:
         try:
-            render()
+            render(display)
         except:
             pass
         finally:
@@ -54,6 +60,7 @@ if __name__ == "__main__":
     if sys.argv[1] == "render":
         render()
     elif sys.argv[1] == "loop":
-        loop()
+        display = sys.argv[2] if 2 < len(sys.argv) else ""
+        loop(display)
     else:
         print("need command: render | loop")
